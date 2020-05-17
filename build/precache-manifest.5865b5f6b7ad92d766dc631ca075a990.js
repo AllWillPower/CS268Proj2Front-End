b061d62cf65f75523f02d69e8c02dfde1eb01387
Willem <VANHAAWA3565@uwec.edu>
2020-05-17 15:27:23 -0500

    Adding npm build

---
+++ build/precache-manifest.5865b5f6b7ad92d766dc631ca075a990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c63467519d1f4c29fdb",
+    "url": "/static/css/main.735afdf0.chunk.css"
+  },
+  {
+    "revision": "5c63467519d1f4c29fdb",
+    "url": "/static/js/main.1fbf8447.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9aff64301f796c25110",
+    "url": "/static/js/2.c89afd1b.chunk.js"
+  },
+  {
+    "revision": "1f65e334c6ab32438c1206c33402eb9d",
+    "url": "/static/media/background-header.1f65e334.png"
+  },
+  {
+    "revision": "41d65ee194cc2598056a775236226e62",
+    "url": "/index.html"
+  }
+];
